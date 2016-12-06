5996e04894267d0196fce6ef1f131677169fed86
Drew Conly <drew.conly@cherwell.com>
2016-12-06 15:27:59 -0700

    add shopping-list form

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -9,9 +9,21 @@ export class ShoppingListService {
     return this.items;
   }
 
+  addItem(item: Ingredient) {
+    this.items.push(item);
+  }
+
   addItems(items: Ingredient[]) {
     // the following prevents a for loop for each item in the array argument
     Array.prototype.push.apply(this.items, items);
+  }
+
+  editItem(oldItem: Ingredient, newItem: Ingredient) {
+    this.items[this.items.indexOf(oldItem)] = newItem;
+  }
+
+  deleteItem(item: Ingredient) {
+    this.items.splice(this.items.indexOf(item), 1);  
   }
 
 }
